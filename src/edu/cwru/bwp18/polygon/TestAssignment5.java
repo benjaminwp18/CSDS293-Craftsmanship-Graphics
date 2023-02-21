c6d223e849115e0ec29550a99042d3b93a1475c5
Benjamin Poulin <bwp18@case.edu>
2023-02-21 14:38:49 -0500

    Use strings in tests

--- src/edu/cwru/bwp18/polygon/TestAssignment5.java
+++ src/edu/cwru/bwp18/polygon/TestAssignment5.java
@@ -10,30 +10,30 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TestAssignment5 {
     @Test
     public void testRectangleGroup() {
-        Set<Rectangle<Integer>> discreteSet = Set.of(
-                Rectangle.of(0, 3, 0, 3),
-                Rectangle.of(5, 6, 5, 6)
+        Set<Rectangle<String>> discreteSet = Set.of(
+                Rectangle.of("0", "3", "0", "3"),
+                Rectangle.of("5", "6", "5", "6")
         );
 
-        Set<Rectangle<Integer>> cornerSet = Set.of(
-                Rectangle.of(0, 3, 0, 3),
-                Rectangle.of(3, 6, 3, 6)
+        Set<Rectangle<String>> cornerSet = Set.of(
+                Rectangle.of("0", "3", "0", "3"),
+                Rectangle.of("3", "6", "3", "6")
         );
 
-        Set<Rectangle<Integer>> twoThirdsSet = Set.of(
-                Rectangle.of(0, 3, 0, 3),
-                Rectangle.of(1, 6, 2, 6),
-                Rectangle.of(-2, 0, -2, 0)
+        Set<Rectangle<String>> twoThirdsSet = Set.of(
+                Rectangle.of("0", "3", "0", "3"),
+                Rectangle.of("1", "6", "2", "6"),
+                Rectangle.of("8", "9", "8", "9")
         );
 
-        Set<Rectangle<Integer>> edgeSet = Set.of(
-                Rectangle.of(0, 3, 0, 3),
-                Rectangle.of(3, 6, 0, 6)
+        Set<Rectangle<String>> edgeSet = Set.of(
+                Rectangle.of("0", "3", "0", "3"),
+                Rectangle.of("3", "6", "0", "6")
         );
 
-        Set<Rectangle<Integer>> overlapSet = Set.of(
-                Rectangle.of(0, 3, 0, 3),
-                Rectangle.of(1, 6, 2, 6)
+        Set<Rectangle<String>> overlapSet = Set.of(
+                Rectangle.of("0", "3", "0", "3"),
+                Rectangle.of("1", "6", "2", "6")
         );
 
         assertFalse(RectangleGroup.from(discreteSet) .isConnected());
